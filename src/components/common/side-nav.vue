8f4f48b38f27805e101343851f56f0e548b2e935
Marvin C <marvin@honeycrm.com>
2021-04-26 00:53:00 -0500

    case

---
+++ src/components/common/side-nav.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="side-nav">
+    <div class="logo">
+      <font-awesome-icon icon="yin-yang" /> 
+    </div>
+    <div class="menu-item">      
+      <router-link
+        to="/">
+        <div class="menu-heading"><font-awesome-icon icon="rss" />Feed</div>
+      </router-link>
+    </div>
+    <div class="menu-item">
+      <router-link
+        to="/combos">
+        <div class="menu-heading"><font-awesome-icon icon="gamepad" />Combos</div>
+      </router-link>
+    </div>
+    <div class="menu-item">
+      <div class="menu-heading"><font-awesome-icon icon="users" />Players</div>
+    </div>
+    <div class="menu-item">
+      <div class="menu-heading"><font-awesome-icon icon="meteor" />Matches</div>
+    </div>
+    <div class="menu-item">
+      <div class="menu-heading"><font-awesome-icon icon="network-wired" />Tournaments</div>
+    </div>
+    <div class="menu-item">
+      <div class="menu-heading"><font-awesome-icon icon="video" />Creators</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SideNav',
+
+  props: {
+
+  },
+
+  methods: {
+
+  }
+
+};
+</script>
+
+<style type="text/css">
+  .side-nav {
+    width: 300px;
+    padding: 80px 30px;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+  }
+
+  .side-nav h2{
+    font-size: 30px;
+    color: #3EB489;
+    margin-bottom: 20px;
+  }
+
+  .side-nav .menu-item .menu-heading{
+    font-size: 20px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    color: #3EB489;
+    margin-bottom: 20px;
+    line-height: 1.2em;
+    font-weight: 300;
+    text-decoration: none;
+  }
+
+  .side-nav .menu-item:hover {
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .side-nav .sub-menu-item {
+    padding-left: 20px;
+    margin-top: 20px;
+  }
+
+  .side-nav .menu-heading svg {
+    margin-right: 15px;
+    width: 25px;
+  }
+
+  .side-nav .logo {
+    display: flex;
+    align-items: center;
+    margin-bottom: 40px;
+    color: #3EB489;
+    text-transform: uppercase;
+    font-size: 20px;
+  }
+
+  .side-nav .logo svg {
+    font-size: 40px;
+    margin-bottom: 50px;
+    font-weight: 300;
+    margin: 0;
+    margin-right: 10px;
+  }
+  .side-nav .logo svg path {
+    fill: #3EB489;
+  }
+</style>
---
+++ src/components/common/top-nav.vue
@@ -0,0 +1,283 @@
+<template>
+    <div class="top-nav">
+      <div class="search-container" placeholder="Search Category">
+        <multiselect 
+          v-model="searchValue" 
+          :options="searchValues" 
+          :close-on-select="true"
+          :clear-on-select="true" 
+          :preserve-search="true" 
+          @input="setSearch()"
+          placeholder="Search..."
+          group-label="category"
+          group-values="values"
+          label="value" 
+          track-by="value" 
+          v-if="!isLoading">
+          <template slot="selection" 
+            slot-scope="{ values, isOpen }">
+            <span class="multiselect__single" 
+              v-if="values.length &amp;&amp; !isOpen">
+              Select Game
+            </span>
+          </template>
+        </multiselect>
+      </div>
+      <!-- <div class="account-btns">
+        <v-btn class="register-btn" @click="openRegisterModal()">Register</v-btn>
+        <register 
+        v-if="isRegisterModalOpen"
+        @register:success="closeRegisterModal()" />
+
+        <v-btn class="login-btn" @click="openLoginModal()">Login</v-btn>
+        <login 
+        v-if="isLoginModalOpen"
+        @register:success="closeLoginModal()" />
+      </div> -->
+      <v-btn class="mx-2 add-btn" fab dark color="cyan" @click="toggleDropDown()" >
+        <v-icon dark>mdi-plus</v-icon>
+      </v-btn>
+      <div class="add-content-dropdown" v-if="isDropDownOpen">
+        <ul>
+          <li v-for="create in createOptions" :key="create.value" @click="openCreateWidget(create.value)">
+            {{create.name}}
+          </li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+import { eventbus } from '@/main';
+// import Register from '@/components/account/register';
+// import Login from '@/components/account/login';
+import GeneralService from '@/services/general-service';
+
+export default {
+  name: 'TopNav',
+
+  components: {
+    // 'register': Register,
+    // 'login': Login
+  },
+  provide() {
+    return {
+        'video': this.video,
+    };
+  },
+  data: () => ({
+    isLoading: true,
+    search: null,
+    isDropDownOpen: false,
+    isRegisterModalOpen: false,
+    isLoginModalOpen: false,
+    searchValue: null,
+    searchValues: [],
+    createOptions:[{
+      name: 'Video',
+      value: 'video'
+    },
+    {
+      name: 'Game',
+      value: 'game'
+    },
+    {
+      name: 'Player',
+      value: 'player'
+    },
+    {
+      name: 'Creator',
+      value: 'creator'
+    },
+    {
+      name: 'Character',
+      value: 'character'
+    },
+    {
+      name: 'Tournament',
+      value: 'tournament'
+    }],
+  }),
+
+  methods: {
+    async getSearch() {
+      const response = await GeneralService.fetchForSearch();
+      var searchValues = response.data.searchValues.map(value => {
+        var searchValue = {
+          id: value._id,
+          value: '',
+          valueType: '',
+        }
+        if(value.GamesPlayed){
+            searchValue.value = value.Name,
+            searchValue.valueType = 'Player'
+        }
+        else if(value.GameId){
+            searchValue.value = value.Name,
+            searchValue.valueType = 'Character'
+        }
+        else if(value.YoutubeUrl){
+            searchValue.value = value.Name,
+            searchValue.valueType = 'ContentCreator'
+        }
+        else if(value.Title){
+            searchValue.value = value.Title,
+            searchValue.valueType = 'Game'
+        }
+
+        return searchValue;
+      });
+
+      this.searchValues = [
+        {
+          category: 'Player',
+          values: searchValues.filter(value => value.valueType === 'Player')
+        },
+        {
+          category: 'Character',
+          values: searchValues.filter(value => value.valueType === 'Character')
+        },
+        {
+          category: 'Content Creator',
+          values: searchValues.filter(value => value.valueType === 'ContentCreator')
+        },
+        {
+          category: 'Game',
+          values: searchValues.filter(value => value.valueType === 'Game')
+        }
+      ]
+      
+      this.isLoading = false;
+    },
+
+    setSearch() {
+      var query = {
+        queryName: this.searchValue.valueType,
+        queryValue: this.searchValue.id
+      }
+      
+      eventbus.$emit('search' , query);
+    },
+
+    openRegisterModal() {
+      this.isRegisterModalOpen = true;
+      this.closeLoginModal();
+    },
+
+    closeRegisterModal() {
+      this.isRegisterModalOpen = false;
+    },
+
+    openLoginModal() {
+      this.isLoginModalOpen = true;
+      this.closeRegisterModal();
+    },
+
+    closeLoginModal() {
+      this.isLoginModalOpen = false;
+    },
+
+    openCreateWidget(createType) {
+      this.$emit('open:createWidget' , createType);
+      this.toggleDropDown();
+    },
+
+    toggleDropDown() {
+      this.isDropDownOpen = !this.isDropDownOpen;
+    },
+  },
+
+  mounted () {    
+    this.getSearch();
+  },
+
+  created() {
+      eventbus.$on('updateSearch', this.getSearch);
+  },
+
+  beforeDestroy() {
+      eventbus.$off('updateSearch', this.getSearch);
+  },
+};
+</script>
+
+<style type="text/css">
+  .top-nav .search-container {
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .top-nav .search-container .multiselect {
+    margin: 0 auto;
+    width: 400px;
+  }
+
+  .top-nav {
+    height: 50px;
+    width: 100%;
+    color: #fff;
+    text-align: center;
+    background: transparent;
+    display: flex;
+    justify-content: center;
+    padding: 0 56px 0 56px;
+    position: relative;
+  }
+
+  .top-nav .add-btn {
+    position: absolute;
+    top: 0;
+    right: 56px;
+    background: #1ab097 !important;
+  }
+
+  .top-nav .add-content-dropdown {
+    position: absolute;
+    top: 100%;
+    margin-top: 20px;
+    right: 35px;
+  }
+
+  .top-nav .add-content-dropdown li {
+    list-style: none;
+    font-size: 40px;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+
+  .top-nav .add-content-dropdown li:hover {
+    opacity: .7;
+  }
+
+  .top-nav .search-container fieldset {
+    background: #fff;
+  }
+
+  .top-nav .search-category,
+  .top-nav .search-input {
+    background: #fff;
+    color: #000;
+    padding: 7px;
+    border-radius: 8px;
+    width: 200px;
+    margin: 0 10px;
+  }
+
+  .top-nav .search-btn {
+    margin-left: 10px;
+  }
+
+  .top-nav .account-btns .v-btn {
+    color: #000;
+    margin: 0 10px;
+    height: 33px;
+  }
+
+  .top-nav .account-btns {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
