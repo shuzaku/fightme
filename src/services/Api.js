bb2b2a55baf9c96b1ff8995dd972511ae8f4f108
Marvin C <marvin@honeycrm.com>
2021-05-02 21:09:12 -0500

    new api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
     return axios.create({
-        baseURL: 'https://fightme-server.herokuapp.com/'
+        baseURL: 'https://fightme-server.herokuapp.com'
     })
 }
