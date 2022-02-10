61aa4ffabccbc36792be683fd9005a31cc3bd9ae
Marvin <mchau@glasshive.com>
2022-02-10 16:43:29 -0600

    api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
     return axios.create({
-        baseURL: ' https://fightme-312602.uc.r.appspot.com'
+        baseURL: 'https://fightme-server.herokuapp.com/'
         // baseURL: 'http://localhost:8082'
     })
 }
